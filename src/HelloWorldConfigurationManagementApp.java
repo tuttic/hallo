f14009a1b03b340005f387243df9f2dfbc029c28
Enoch Mabombo Ngweyi <enochmabombo@Enochs-MacBook-Pro.local>
2024-09-13 17:34:07 +0200

    Hallo

---
+++ src/HelloWorldConfigurationManagementApp.java
@@ -0,0 +1,15 @@
+public class HelloWorldConfigurationManagementApp {
+
+    //main+TAB oder psvm+TAP
+    public static void main(String[] args) {
+        //sout+TAP
+        System.out.println("Hello World Configurations Management");
+
+        System.out.println("Enoch Mabombo");
+
+        System.out.println("Konfigurationsmanagement");
+
+    }
+
+}
+
